4feb89b41ca5178bb701d60e5ea3445b78f52918
Larry <rwc25@uw.edu>
2016-01-31 22:08:54 -0800

    The Dictionary and the IO for the dictionary.

--- Vespar/VoiceRecogTest/VoiceRecogTest/Form1.cs
+++ Vespar/VoiceRecogTest/VoiceRecogTest/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Speech.Recognition;
+using System.IO;
 
 namespace VoiceRecogTest
 {
@@ -19,6 +20,25 @@ namespace VoiceRecogTest
             InitializeComponent();
         }
 
+        private static void Dictionary()
+        {
+            try
+            {   // Open the text file using a stream reader.
+                using (StreamReader sr = new StreamReader("wordsEn.txt"))
+                {
+                    // Read the stream to a string, and write the string to the console.
+                    String line = sr.ReadToEnd();
+                    Console.WriteLine(line);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+            
+        }
+
         private void btnEnable_Click(object sender, EventArgs e)
         {
             recEngine.RecognizeAsync(RecognizeMode.Multiple);
@@ -27,8 +47,12 @@ namespace VoiceRecogTest
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            StreamReader words = new StreamReader("wordsEn.txt");
+            String line = words.ReadToEnd().ToString();
+            line.Split('\n');
+
             Choices commands = new Choices();
-            commands.Add(new string[] { "Say Hello", "print my name" });
+            commands.Add(new string[] { line });
             GrammarBuilder gBuilder = new GrammarBuilder();
             gBuilder.Append(commands);
             Grammar grammer = new Grammar(gBuilder);
