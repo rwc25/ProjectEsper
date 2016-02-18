33383644a515bca528d258039bc54824043d919b
mcdr25 <mcdr25@uw.edu>
2016-02-17 22:27:15 -0800

    this is a more professional looking copy of the code. Added the function of parsing through a text file, removed the switch statements and added the function to store the user input that is recognized and written into a text file

--- Jarvis/Jarvis/Form1.cs
+++ Jarvis/Jarvis/Form1.cs
@@ -15,8 +15,10 @@ using System.IO;
 /*
 Author              Date                Description
 Mark Dela Rosa      1/10/16 - 1/15/16   Pseudocode for start of project
-                    2/10/16             Implementing a feature to input what user says into a string and send that string to a text file.
-                                        Adding a counter to how many times the user says a specific word or phrase.
+Mark Dela Rosa      2/10/16             Implementing a feature to input what user says into a string and send that string to a text file.
+Mark Dela Rosa                          Adding a counter to how many times the user says a specific word or phrase.
+Mark Dela Rosa      2/17/16             Removed the switch statements. Added a function to read a text file line by line. (Dictionary)
+                                        Added a function to store user input and write it to a text file. (Data/Database)                               
 */
 
 
@@ -29,39 +31,56 @@ namespace Jarvis
             InitializeComponent();
         }
 
+        //creates computer voice
         SpeechSynthesizer jSynth = new SpeechSynthesizer();
+        //creates temp dictionary for computer voice to speak
         PromptBuilder jBuilder = new PromptBuilder();
+        //creates speech recognition instance
         SpeechRecognitionEngine jRecognize = new SpeechRecognitionEngine();
         
 
-
+        //n/a
         private void Form1_Load(object sender, EventArgs e)
         {
 
         } 
+        //allows for computer to speak whatever is typed in the richtextbox
         private void button1_Click(object sender, EventArgs e)
         {
             jBuilder.ClearContent();
             jBuilder.AppendText(richTextBox1.Text);
             jSynth.Speak(jBuilder);
         }
-
-        //testing reading file 
+        /*
+        //testing ideas for a text file 
         //string[] commands = new string[];
         //eventually will be replaced with a database
-        StreamReader sR = new StreamReader(@"C:\Users\Batman\Desktop\projectvespar\test.txt");
-        // ///////////////////////
+        //StreamReader sR = new StreamReader(@"C:\Users\Batman\Desktop\projectvespar\test.txt");
+        */
 
-
-
+        //Dictionary via a text file
+        StreamReader sR = new StreamReader(@"C:\Users\Batman\Desktop\mydictionary.txt");
+        
+        //start button 
         private void button2_Click(object sender, EventArgs e)
         {
 
             button2.Enabled = false;
             button3.Enabled = true;
             Choices jList = new Choices();
+            string readTextLine = "";
+            readTextLine = sR.ReadLine();
+            string testing = "";
+            while ((readTextLine = sR.ReadLine()) != null)
+            {
+                jList.Add(new string[] { readTextLine });
+                testing += readTextLine;
+
+            }
+            
             //TEsting////////////////////////
-            /*string readTextLine = "";
+            /*
+            string readTextLine = "";
             readTextLine = sR.ReadLine();
             string testing = "";
             while ((readTextLine = sR.ReadLine()) != null)
@@ -71,11 +90,8 @@ namespace Jarvis
                 testing += readTextLine + "|";
             }
             */
-
-            /*
-            // Make code from user input to -> store what they say and write it to a text file
-            ///////////////////////////////
-            */
+            
+            
             jList.Add(new string[] { "open window", "close window", "change to woman", "change to man", "testing" });
             Grammar gr = new Grammar(new GrammarBuilder(jList));
             
@@ -88,51 +104,36 @@ namespace Jarvis
                     jRecognize.SpeechRecognized += JRecognize_SpeechRecognized;
                     jRecognize.SetInputToDefaultAudioDevice();
                     jRecognize.RecognizeAsync(RecognizeMode.Multiple);
+                    
                     //testing to see if it reads text file and commands are there
                     MessageBox.Show("*Test* Commands Available" + "open window | close window | change to woman | change to man | testing");
-
-
+                    
                 }
                 catch
                 {
                     return;
                 }
-
-            }
-           
+            }           
         }
-
-      
+        
+        //Where user input will be written to
         StreamWriter sW = new StreamWriter(@"C:\Users\Batman\Desktop\projectvespar\writetome.txt", true);
         private void JRecognize_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
         {
-
             /*
-            Test 2/10/16
-            
-                    
-            string fileName = @"C:\Users\Batman\Desktop\projectvespar\writefile.txt"
-            
-            StreamWriter sW = new StreamWriter(fileName, true);
-            sW.WriteLine(e.Result.Text);
-            
-            
-            
-             do
-            {
-                if (e.Result.Text != "finish" && e.Result.Text != "") 
-                {
-                    sW.WriteLine(e.Result.Text);
-                }
-            } while (e.Result.Text != "finish");
-                sW.Close();
-            
-            
+            This will have to be configured later down the road but right now it does 
+            its job of writing the user input into a text document based on the dictionary.
             */
 
+            //this writes what the user says onto the GUI
+            richTextBox1.AppendText(e.Result.Text.ToString());
+            //this writes what the users says into a text document
+            sW.Write(e.Result.Text.ToString());            
+            
+            /*
+            //these are test switch statements to see if the user input 
+            //could be regulated based on test cases
 
-            //probably will replace with another file e.Result.Text will be replaced
-            //with an array or database similar to one above
             switch (e.Result.Text)
             {
                 case ("exit"):
@@ -164,22 +165,11 @@ namespace Jarvis
                     sW.Write(" " + e.Result.Text.ToString() + "|");
                     
                     break;
-
-            }
-            /*
-            if (e.Result.Text == "exit")
-            {
-                Application.Exit();
-            } else if(e.Result.Text == "")
-            {
-            }
-            else
-            {
-                richTextBox1.Text = richTextBox1.Text + " " + e.Result.Text.ToString();
             }
             */
+            
         }
-
+        //Stop button
         private void button3_Click(object sender, EventArgs e)
         {
             jRecognize.RecognizeAsyncStop();
