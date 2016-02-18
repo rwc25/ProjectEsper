ce1fd97b3c6809887c0942c61602c4d3df23a7c6
Jonathan <Jonathan>
2016-02-17 19:23:45 -0800

    updated version of the firefox sikuli program

--- Firefox.sikuli/Firefox.py
+++ Firefox.sikuli/Firefox.py
@@ -1,6 +1,9 @@
 if exists("1455035351562.png") :
     
     doubleClick("1455035351562.png") 
+elif exists("1455680924820.png"):
+    doubleClick("1455680924820.png")
+    
 else:
-    doubleClick("1455046035914.png")
+    click("1455046035914.png")
 
