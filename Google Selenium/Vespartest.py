11f1da87a397d56fd8e29e9dbf3500df68029a7c
moreiro <maeo@uw.edu>
2016-02-25 13:24:17 -0800

    VESPAR test 2

--- Google Selenium/Vespartest.py
+++ Google Selenium/Vespartest.py
@@ -4,7 +4,28 @@ path = 'wordfiletest.txt'
 
 wordfiletest = open(path, 'r+')
 while True:
-    text = wordfiletest.read()
-    if 'Firefox' in text: #FireFox will be replaced with the file input
-        print text
-    os.system('C:\Github\Project_Esper\Sikuli\\runsikulix -r' + 'C:\Github\Project_Esper\SikuliScripts\\' + text + '.sikuli')
+    text = wordfiletest.readline()
+    #while 'Firefox' in text: #FireFox will be replaced with the file input
+     #   print text
+    #    os.system('C:\Github\Project_Esper\Sikuli\\runsikulix -r' + 'C:\Github\Project_Esper\SikuliScripts\\' + text + '.sikuli')
+    #text = wordfiletest.next()
+    #else:
+     #   break
+
+    for line in wordfiletest:
+        print line
+        os.system('C:\Github\Project_Esper\Sikuli\\runsikulix -r' + 'C:\Github\Project_Esper\SikuliScripts\\' + text + '.sikuli')
+
+
+
+#if wordfiletest.read() in text:
+ #   print text
+
+#while True:
+    #for index in path:
+#nextword = wordfiletest.readline()
+#if wordfiletest.readline() == 'Firefox':
+ #   print nextword
+            #os.system('C:\Github\Project_Esper\Sikuli\\runsikulix -r' + 'C:\Github\Project_Esper\SikuliScripts\\' + nextword + '.sikuli')
+    #extword = wordfiletest.next()
+
