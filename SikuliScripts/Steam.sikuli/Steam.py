304468d6f410b71b85c36c91f8015c97a4cb2e50
Jonathan <Jonathan>
2016-02-24 21:02:21 -0800

    sikuli scripts for Steam, File Explorer, Back and Foward buttons on file explorer

---
+++ SikuliScripts/Steam.sikuli/Steam.py
@@ -0,0 +1,17 @@
+if exists("1456373887713.png"):
+    doubleClick("1456373911891.png")
+elif exists("1456373973105.png"):
+    click("1456374002352.png")
+else:
+
+    click("1456374119103.png")
+    wait(2)
+    type('Steam')
+    exists("1456374243209.png")
+    click("1456374243209.png")
+    
+    
+    
+    
+    
+    
