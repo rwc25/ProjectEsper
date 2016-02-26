6b2b8568e51a9e9241dd924e381c7d860875d88a
moreiro <maeo@uw.edu>
2016-02-25 20:11:03 -0800

    Working Selenium - Opens files, does not read command words

--- Google Selenium/Vespartest.py
+++ Google Selenium/Vespartest.py
@@ -2,19 +2,25 @@ import os.path
 
 path = 'wordfiletest.txt'
 
-wordfiletest = open(path, 'r+')
-while True:
-    text = wordfiletest.readline()
-    #while 'Firefox' in text: #FireFox will be replaced with the file input
-     #   print text
-    #    os.system('C:\Github\Project_Esper\Sikuli\\runsikulix -r' + 'C:\Github\Project_Esper\SikuliScripts\\' + text + '.sikuli')
-    #text = wordfiletest.next()
-    #else:
-     #   break
+'''wordfiletest = open(path, 'r+')
+text = wordfiletest.readline()
+while 'Firefox' in text: #FireFox will be replaced with the file input
+    print text
+    os.system('C:\Github\Project_Esper\Sikuli\\runsikulix -r' + 'C:\Github\Project_Esper\SikuliScripts\\' + text + '.sikuli')
+    text = wordfiletest.next()'''
 
-    for line in wordfiletest:
-        print line
-        os.system('C:\Github\Project_Esper\Sikuli\\runsikulix -r' + 'C:\Github\Project_Esper\SikuliScripts\\' + text + '.sikuli')
+
+wordfiletest = open(path, 'r+')
+while 1:
+    line = wordfiletest.readline()
+    if not line:
+        break
+    print line
+    os.system('C:\Github\Project_Esper\Sikuli\\runsikulix -r' + 'C:\Github\Project_Esper\SikuliScripts\\' + line + '.sikuli')
+
+#for line in wordfiletest:
+#    print line
+#    os.system('C:\Github\Project_Esper\Sikuli\\runsikulix -r' + 'C:\Github\Project_Esper\SikuliScripts\\' + text + '.sikuli')
 
 
 
