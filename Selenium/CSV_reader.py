25940daf29b2d94a05c0aa15bc49b750b6db9124
moreiro <maeo@uw.edu>
2016-02-28 19:34:24 -0800

    UploadComment

--- Selenium/CSV_reader.py
+++ Selenium/CSV_reader.py
@@ -1,7 +1,23 @@
 import csv
+path = 'wordfiletest.txt'
 
-with open('Action_List.csv', 'rb') as f:
-    reader = csv.reader(f)
-    for row in reader:
-        content = list(row[i] for i in [0])
-        print content
+
+wordfiletest = open(path, 'r+')
+while 1:
+    line = wordfiletest.readline()
+    if not line:
+        break
+    print line
+
+    with open('Action_List.csv', 'rb') as f: #looks at Action list
+        reader = csv.reader(f)
+        for row in reader:
+            column = list(row[i] for i in [0])
+            if line ==
+
+
+
+   # os.system('D:\Git\Sikuli\\runsikulix -r' + 'D:\Git\SikuliScripts\\' + line + '.sikuli') #reads filename
+
+
+
--- Selenium/Vespartest.py
+++ Selenium/Vespartest.py
@@ -2,14 +2,6 @@ import os.path
 
 path = 'wordfiletest.txt'
 
-'''wordfiletest = open(path, 'r+')
-text = wordfiletest.readline()
-while 'Firefox' in text: #FireFox will be replaced with the file input
-    print text
-    os.system('C:\Github\Project_Esper\Sikuli\\runsikulix -r' + 'C:\Github\Project_Esper\SikuliScripts\\' + text + '.sikuli')
-    text = wordfiletest.next()'''
-
-
 wordfiletest = open(path, 'r+')
 while 1:
     line = wordfiletest.readline()
